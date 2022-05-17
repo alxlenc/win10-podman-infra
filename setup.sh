4ccf434f17d325d149fad19a17b89450bdb6e1c0
Alex Serna <asernabo@redhat.com>
2022-05-17 17:22:47 +0200

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 zipped_box="win10Edge.box.zip"
 box_name="win10Edge.box"
 
-if [[ ! -f $box_name ]] then
-    echo -e "\nFetching Windows box\n"
+if [[ ! -f $box_name ]]; then
+    echo -e "\nFetching Windows box...\n"
     curl https://az792536.vo.msecnd.net/vms/VMBuild_20190311/Vagrant/MSEdge/MSEdge.Win10.Vagrant.zip -o $zipped_box
 
     echo -e "\nUnzipping and renaming\n"
@@ -12,8 +12,10 @@ if [[ ! -f $box_name ]] then
     mv "$unzipped_file" "$box_name"
 fi
 
-echo "Adding box"
+echo "Adding box from local file"
 vagrant box add win10 ./$box_name
+echo -e "\nProvisioning environment. Be patient, this might take up to 1 hour.\n"
+sleep 3
 vagrant up
-echo "Done"
+echo "Done."
 
