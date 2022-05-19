e920c13161046e30981f6740a4bbe2e0a0e9ac9f
Alex Serna <asernabo@redhat.com>
2022-05-19 10:04:43 +0200

    fix wih-share-folder script

--- win-share-folder.sh
+++ win-share-folder.sh
@@ -11,4 +11,4 @@ win_vm=$(vboxmanage list vms | grep podman-infra_w10 | awk '{print $1}' | tr -d 
 echo -e "\nMounting $1 dir to $win_vm as $curr_dir\n"
 vboxmanage sharedfolder remove $win_vm --name $curr_dir  --transient
 vboxmanage sharedfolder add $win_vm --name $curr_dir --hostpath $1 --transient
-echo  "Run the following command on windows: net use U: \\\\VBOXSVR\\"${curr_dir
+echo  "Run the following command on windows: net use U: \\\\VBOXSVR\\"${curr_dir}
