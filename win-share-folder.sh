7084023d88973d7cb5b5a1269c29ed96a7f9c472
Alex Serna <asernabo@redhat.com>
2022-05-19 11:07:38 +0200

    automount local folder  on drive U:

--- win-share-folder.sh
+++ win-share-folder.sh
@@ -8,7 +8,17 @@ fi
 curr_dir=$(basename $1)
 win_vm=$(vboxmanage list vms | grep podman-infra_w10 | awk '{print $1}' | tr -d '"' )
 
-echo -e "\nMounting $1 dir to $win_vm as $curr_dir\n"
+echo "Sharing $1 dir with $win_vm as $curr_dir"
+echo
 vboxmanage sharedfolder remove $win_vm --name $curr_dir  --transient
 vboxmanage sharedfolder add $win_vm --name $curr_dir --hostpath $1 --transient
-echo  "Run the following command on windows: net use U: \\\\VBOXSVR\\"${curr_dir}
+echo
+echo "Mounting $1 on windows drive U:"
+vagrant winrm w10  \
+  -c "ECHO Y|NET USE U: /DELETE" \
+  -c  "NET USE U: \\\\VBOXSVR\\$curr_dir"
+
+echo
+echo "Done."
+
+
